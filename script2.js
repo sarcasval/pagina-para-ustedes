904f4abb0c84b301e2fa03bb40b230369a6db8a6
sarcasval <s.castillov2@uniandes.edu.co>
2026-03-05 18:54:07 +0000

    5mar

--- script2.js
+++ script2.js
@@ -1,51 +1,44 @@
-// Listas de imágenes
 const listaOjos = [
-  "assets/1.jpg",
-  "assets/2.jpg",
-  "assets/3.png",
-];
-const listaNarices = [
- "../base-bestiario/assets/img/tronco1.jpg",
-  "../base-bestiario/assets/img/tronco2.jpg",
-  "../base-bestiario/assets/img/tronco3.jpg",
-];
-const listaBocas = [
-  "../base-bestiario/assets/img/patas1.jpg",
-  "../base-bestiario/assets/img/patas2.jpg",
-  "../base-bestiario/assets/img/patas3.png",
+"assets/1.jpg",
+"assets/2.jpg",
+"assets/3.png"
 ];
 
-// Obtenemos los contenedores de las imágenes del HTML usando los IDs
+const listaNarices = [
+"assets/img/tronco1.jpg",
+"assets/img/tronco2.jpg",
+"assets/img/tronco3.jpg"
+];
+
+const listaBocas = [
+"assets/img/patas1.jpg",
+"assets/img/patas2.jpg",
+"assets/img/patas3.png"
+];
+
 const ojos = document.getElementById("ojo");
 const nariz = document.getElementById("nariz");
 const boca = document.getElementById("boca");
 const boton = document.getElementById("boton");
 
-// Inicializamos las variables de los números aleatorios
-let ojosAleatorio = 0;
-let narizAleatorio = 0;
-let bocaAleatorio = 0;
-
-// Función para generar un número aleatorio entre dos valores
-function numeroAleatorio(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
+function numeroAleatorio(min,max){
+return Math.floor(Math.random()*(max-min)+min);
 }
 
-// Función para crear una nueva bestia con tres imágenes elegidas aleatoriamente
-function generarBestia() {
-  ojosAleatorio = numeroAleatorio(0, listaOjos.length);
-  narizAleatorio = numeroAleatorio(0, listaNarices.length);
-  bocaAleatorio = numeroAleatorio(0, listaBocas.length);
+function generarBestia(){
 
-  // Asignamos la nueva fuente (source) a cada imagen
-  ojos.src = `${listaOjos[ojosAleatorio]}`; //ruta + listaCabezas[cabezaAleatorio];
-  nariz.src = `${listaNarices[narizAleatorio]}`;
-  boca.src = `${listaBocas[bocaAleatorio]}`;
+const ojosAleatorio = numeroAleatorio(0,listaOjos.length);
+const narizAleatorio = numeroAleatorio(0,listaNarices.length);
+const bocaAleatorio = numeroAleatorio(0,listaBocas.length);
+
+ojos.src = listaOjos[ojosAleatorio];
+nariz.src = listaNarices[narizAleatorio];
+boca.src = listaBocas[bocaAleatorio];
+
 }
 
-// Generamos un nuevo collage cada vez que hacemos click en el botón "mezclar"
-boton.addEventListener("click", function () {
-  generarBestia();
+boton.addEventListener("click",function(){
+generarBestia();
 });
 
 generarBestia();
