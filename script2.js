ae4726f5ecfb35276a3dc1f4597fd92b32bf6029
sarcasval <s.castillov2@uniandes.edu.co>
2026-03-06 17:12:51 +0000

    musicafondo

--- script2.js
+++ script2.js
@@ -1,44 +1,47 @@
 const listaOjos = [
-"assets/1.jpg",
-"assets/2.jpg",
-"assets/3.png"
+  "assets/1.jpg",
+  "assets/2.jpg",
+  "assets/3.jpg",
 ];
 
 const listaNarices = [
-"assets/img/tronco1.jpg",
-"assets/img/tronco2.jpg",
-"assets/img/tronco3.jpg"
+  "assets/tronco1.jpg",
+  "assets/tronco2.jpg",
+  "assets/tronco3.jpg",
 ];
 
 const listaBocas = [
-"assets/img/patas1.jpg",
-"assets/img/patas2.jpg",
-"assets/img/patas3.png"
+  "assets/patas1.jpg",
+  "assets/patas2.jpg",
+  "assets/patas3.jpg",
 ];
 
 const ojos = document.getElementById("ojo");
 const nariz = document.getElementById("nariz");
 const boca = document.getElementById("boca");
 const boton = document.getElementById("boton");
+const musica = document.getElementById("musicaFondo");
 
-function numeroAleatorio(min,max){
-return Math.floor(Math.random()*(max-min)+min);
+function numeroAleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
-function generarBestia(){
+function generarBestia() {
+  const ojosAleatorio = numeroAleatorio(0, listaOjos.length);
+  const narizAleatorio = numeroAleatorio(0, listaNarices.length);
+  const bocaAleatorio = numeroAleatorio(0, listaBocas.length);
 
-const ojosAleatorio = numeroAleatorio(0,listaOjos.length);
-const narizAleatorio = numeroAleatorio(0,listaNarices.length);
-const bocaAleatorio = numeroAleatorio(0,listaBocas.length);
-
-ojos.src = listaOjos[ojosAleatorio];
-nariz.src = listaNarices[narizAleatorio];
-boca.src = listaBocas[bocaAleatorio];
-
+  ojos.src = listaOjos[ojosAleatorio];
+  nariz.src = listaNarices[narizAleatorio];
+  boca.src = listaBocas[bocaAleatorio];
 }
 
-boton.addEventListener("click",function(){
-generarBestia();
+boton.addEventListener("click", function () {
+  if (musica.paused) {
+    musica.play();
+  }
+
+  generarBestia();
 });
 
 generarBestia();
